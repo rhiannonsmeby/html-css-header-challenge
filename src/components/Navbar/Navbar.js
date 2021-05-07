86f037dd6f546753e32916e55b65a7843b00dc16
Rhiannon Smeby <smebyrhi@gmail.com>
2021-05-07 11:40:19 -0500

    main desktop nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import {NavLink} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faBars, faTimes, faSearch} from '@fortawesome/free-solid-svg-icons';
+import {faBars, faTimes, faSearch, faShieldAlt, faQuestionCircle, faMapMarkerAlt, faUserAlt, faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+import Logo from '../../assets/lincolnlogo.png';
 import './Navbar.css';
 
 export default function Navbar() {
@@ -21,15 +22,22 @@ export default function Navbar() {
             </div>   
         </div>
         <nav className={navToggle ? 'open' : 'closed'}>
+            <ul className='secondary-nav'>
+                <li className='nav-item'><FontAwesomeIcon icon={faShieldAlt} className={'search'}/></li>
+                <li className='nav-item'><FontAwesomeIcon icon={faQuestionCircle} className={'search'}/></li>
+                <li className='nav-item'><FontAwesomeIcon icon={faMapMarkerAlt} className={'search'}/></li>
+                <li className='nav-item'><FontAwesomeIcon icon={faUserAlt} className={'search'}/></li>
+                <li className='nav-item'><FontAwesomeIcon icon={faShoppingCart} className={'search'}/></li>
+            </ul>
             <ul className='nav'>
-                <li className='nav-item-title'><NavLink exact to={`/`}>LINCOLN ELECTRIC</NavLink></li>
+                <li className='nav-item-title'><NavLink exact to={`/`}><img className='title' src={Logo} alt="Lincoln Electric Logo"/></NavLink></li>
                 <li className='nav-item'><NavLink to={`/solutions`} activeClassName='navActive'>SOLUTIONS</NavLink></li>
                 <li className='nav-item'><NavLink to={`/products`} activeClassName='navActive'>PRODUCTS</NavLink></li>
                 <li className='nav-item'><NavLink to={`/automation`} activeClassName='navActive'>AUTOMATION</NavLink></li>
                 <li className='nav-item'><NavLink to={`/education`} activeClassName='navActive'>EDUCATION</NavLink></li>
                 <li className='nav-item'><NavLink to={`/resources`} activeClassName='navActive'>RESOURCES</NavLink></li>
-                <li className='nav-item-search'>Search <FontAwesomeIcon icon={faSearch} className={'search'}/></li>
             </ul>
+            <p className='nav-item-search'>Search <FontAwesomeIcon icon={faSearch} className={'search'}/></p>
         </nav>
       </div>
     );
