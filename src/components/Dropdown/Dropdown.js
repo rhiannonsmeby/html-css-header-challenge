648c9f167846bb5542130580c8d6a2786537bde1
Rhiannon Smeby <smebyrhi@gmail.com>
2021-05-09 15:00:50 -0500

    mobile dropdown menu

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
+import {faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons';
 import './Dropdown.css';
 
-export default function Dropdown() {
+function Dropdown(props) {
+    
     return (
         <div className='dropdown-container'>
-            <ul className='dropdown'>
-                <li className="dropdownli">Soultions By Industry</li>
-                <li className="dropdownli">Solutions By Process</li>
-                <li className="dropdownli">Software Solutions</li>
-                <li className="dropdownli">Services</li>
-            </ul>
+            <div className='dropdown-back'>
+                <p><FontAwesomeIcon icon={faChevronLeft}/> GO BACK</p> 
+            </div>
+            <div className='dropdown'>
+                <div className="dropdownli"><p>Soultions By Industry <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} className='icon-nav'/></span></p></div>
+                <div className="dropdownli"><p>Solutions By Process <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} className='icon-nav'/></span></p></div>
+                <div className="dropdownli"><p>Software Solutions <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} className='icon-nav'/></span></p></div>
+                <div className="dropdownli"><p>Services <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} className='icon-nav'/></span></p></div>
+            </div>
         </div>
     );
 }
+
+Dropdown.propTypes = {
+    history: PropTypes.object
+}
+
+
+export default Dropdown;
