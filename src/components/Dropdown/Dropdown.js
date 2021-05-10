352caf47789060b520b118139b22d70f3211c007
Rhiannon Smeby <smebyrhi@gmail.com>
2021-05-09 19:34:39 -0500

    go back working

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,29 +1,21 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import PropTypes from 'prop-types';
 import {faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons';
 import './Dropdown.css';
 
-function Dropdown(props) {
-    
+export default function Dropdown(props) {
     return (
         <div className='dropdown-container'>
-            <div className='dropdown-back'>
-                <p><FontAwesomeIcon icon={faChevronLeft}/> GO BACK</p> 
+            <div className='dropdown-back' onClick={props.dropdownToggle}>
+                <NavLink to='/navigation' className='go-back'><p><FontAwesomeIcon icon={faChevronLeft}/> GO BACK</p></NavLink> 
             </div>
             <div className='dropdown'>
-                <div className="dropdownli"><p>Soultions By Industry <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} className='icon-nav'/></span></p></div>
-                <div className="dropdownli"><p>Solutions By Process <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} className='icon-nav'/></span></p></div>
-                <div className="dropdownli"><p>Software Solutions <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} className='icon-nav'/></span></p></div>
-                <div className="dropdownli"><p>Services <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} className='icon-nav'/></span></p></div>
+                <div className="dropdownli"><p>Soultions By Industry <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} /></span></p></div>
+                <div className="dropdownli"><p>Solutions By Process <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} /></span></p></div>
+                <div className="dropdownli"><p>Software Solutions <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} /></span></p></div>
+                <div className="dropdownli"><p>Services <span className='mobile-element' style={{'alignSelf': 'right'}}><FontAwesomeIcon icon={faChevronRight} /></span></p></div>
             </div>
         </div>
     );
 }
-
-Dropdown.propTypes = {
-    history: PropTypes.object
-}
-
-
-export default Dropdown;
